62bbd416abf14a24e71e14de055e31483468dfe3
mallahajay43 <mallahajay43@gmail.com>
2026-05-24 12:02:19 +0000

    project-inialization

---
+++ src/main/java/com/mallahajay43/coding_shuttle_learning/CodingShuttleLearningApplication.java
@@ -0,0 +1,13 @@
+package com.mallahajay43.coding_shuttle_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingShuttleLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingShuttleLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mallahajay43/coding_shuttle_learning/CodingShuttleLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallahajay43.coding_shuttle_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingShuttleLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
